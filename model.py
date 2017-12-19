f60eeb08544defd7c057a0f5d9be4022830ab76a
denmag <dennis.magnusson98@gmail.com>
2017-12-19 13:16:27 -0800

    Christmas update

--- model.py
+++ model.py
@@ -5,24 +5,21 @@ from keras.layers import Conv2D, Conv2DTranspose, Activation, Dense, Flatten
 
 import preprocessor
 
-def step(x):
-  return 0 if x < 0 else 1
-
 class Model:
   def __init__(self):
     self.discriminator = self.create_discriminator()
-    #self.generator = self.create_generator()
+    self.generator = self.create_generator()
     data = preprocessor.read_dataset("./data")
     self.train(data, 10)
 
   def train(self, dataset, batch_size):
     dataset_length = dataset.shape[3]
-    #positives = self.generator.fit()
     real_songs = dataset
-    #gen_songs  = self.generator.predict(self.noise(dataset_length))#TODO Uncomment
-    gen_songs = self.noise(dataset_length)
-    #TODO Shuffle?
+    gen_songs  = self.round(self.generator.predict(self.noise(dataset_length)))
     print(gen_songs.shape)
+    print(real_songs.shape)
+    #gen_songs = self.noise(dataset_length)
+    #print(gen_songs.shape)
     x = np.concatenate((gen_songs, real_songs), axis=3)
     y = np.concatenate(([[1, 0]]*dataset_length, [[0, 1]]*dataset_length), axis=0)#[1, 0] -> computer, [0, 1] -> human
     perm = np.random.permutation(len(x))
@@ -30,6 +27,8 @@ class Model:
     y = y[perm]
     self.discriminator.fit(x, y, batch_size=2*batch_size)
     predictions = self.discriminator.predict(x)
+    #TODO Error.
+    #Update weights of generator. Somehow.
     print(predictions)
 
 
@@ -52,17 +51,20 @@ class Model:
     
   def create_generator(self):
     model = Sequential([
-      Conv2DTranspose(10, (32, 32), input_shape=(88, 64, 1)),
+      Conv2DTranspose(1, (8, 8), input_shape=(88, 64, 1)),#FIXME dims seem to reduce with 7 for every layer.
       Activation('relu'),
-      Conv2DTranspose(10, (32, 32)),
+      Conv2DTranspose(1, (8, 8)),
       Activation('relu'),
-      Conv2DTranspose(10, (32, 32)),
-      Activation(step)
+      Conv2DTranspose(1, (8, 8)),
+      Activation('sigmoid')
     ])
 
     model.compile(loss='binary_crossentropy', optimizer='Adam')
     model.summary()
     return model
+
+  def round(self, x):
+    return np.where(x < 0.5, 0, 1)
 
   def generate(self):
     songs = self.generator.predict(noise(batch_size))
