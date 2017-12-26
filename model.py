6f0d137436362bcdc7f49b4c53069c6131dfcacf
d <dennis.magnusson98@gmail.com>
2017-12-26 21:40:38 +0100

    It works

--- model.py
+++ model.py
@@ -1,35 +1,55 @@
 import numpy as np
 
-from keras.models import Sequential
-from keras.layers import Conv2D, Conv2DTranspose, Activation, Dense, Flatten, Dropout
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, Conv2DTranspose, Activation, Dense, Flatten, Dropout, Input
 
 import preprocessor
 
-class Model:
+class GAN:
   def __init__(self):
-    self.discriminator = self.create_discriminator()
-    self.generator = self.create_generator()
-    data = preprocessor.read_dataset("./data", length=20)
-    self.train(data, 10)
+    self.D = self.create_discriminator()
+    self.D.compile(loss='binary_crossentropy', optimizer='Adam')
+    self.D.summary()
+    self.D.trainable = False
+    self.G = self.create_generator()
+    self.G.compile(loss='binary_crossentropy', optimizer='Adam')
+    self.G.summary()
+    inp = Input(shape=(66, 46, 20,))
+    #Model(inp, self.D(self.G(inp)))
+    self.GAN = Model(inp, self.D(self.G(inp)))
+    self.GAN.compile(loss="binary_crossentropy", optimizer="Adam")
+    data = preprocessor.read_dataset("./data", length=60)
+    self.train(data, 20)
 
   def train(self, dataset, batch_size):
     dataset_length = dataset.shape[0]
     real_songs = dataset
-    gen_songs  = self.round(self.generator.predict(self.noise(dataset_length, self.generator.input_shape)))
-    print(gen_songs.shape)
-    print(real_songs.shape)
-    #gen_songs = self.noise(dataset_length)
-    print(gen_songs.shape)
-    x = np.concatenate((gen_songs, real_songs), axis=0)
-    y = np.concatenate(([[1, 0]]*dataset_length, [[0, 1]]*dataset_length), axis=0)#[1, 0] -> computer, [0, 1] -> human
-    perm = np.random.permutation(len(x))
-    x = x[perm]
-    y = y[perm]
-    self.discriminator.fit(x, y, batch_size=2*batch_size)
-    predictions = self.discriminator.predict(x)
-    #TODO Error.
-    #Update weights of generator. Somehow.
-    print(predictions)
+
+    input_shape = (20, 66, 46, 20)
+    #x = np.concatenate((gen_songs, real_songs), axis=0)
+    #perm = np.random.permutation(len(x))
+    #x = x[perm]
+    #y = y[perm]
+    for i in range(0, len(dataset), dataset_length):
+      n = self.noise(batch_size, input_shape)
+      print(n.shape)
+      gen_songs  = self.round(self.G.predict(self.noise(batch_size, input_shape)))#Generate songs
+      G_loss = self.D.train_on_batch(gen_songs, np.array([[0]]*batch_size))#Train on fake songs. Returns integer loss in form of an integer.
+      D_loss = self.D.train_on_batch(dataset[i:i+batch_size], np.array([[1]]*batch_size))
+
+      print(self.D.predict(gen_songs))
+
+      gen_loss = self.GAN.train_on_batch(self.noise(batch_size, input_shape), np.ones((batch_size, 1)))
+
+      #gan_loss = 1 - self.D.train_on_batch(gen_songs, np.array([0, 1]*batch_size))#TODO Check if this works
+      #self.D.train_on_batch(x, y)
+      #predictions = self.D.predict(x)
+      #self.GAN.train_on_batch(x, predictions)
+      #TODO Error.
+      #Update weights of G. Somehow.
+      print(G_loss)
+      print(D_loss)
+      print(gen_loss)
 
 
   def create_discriminator(self):
@@ -41,15 +61,12 @@ class Model:
       Dropout(0.3),
       Activation('relu'),
       Flatten(),
-      Dense(20),
-      Activation('linear'),
       Dropout(0.3),
-      Dense(2),
-      Activation('softmax')
+      Dense(1),
+      Activation('sigmoid')
     ])
 
-    model.compile(loss='categorical_crossentropy', optimizer='Adam')
-    model.summary()
+    #inp = Input(shape=(88, 64, 1))
     return model
     
   def create_generator(self):
@@ -62,21 +79,14 @@ class Model:
       Activation('sigmoid')
     ])
 
-    model.compile(loss='binary_crossentropy', optimizer='Adam')
-    model.summary()
     return model
 
   def round(self, x):
     return np.where(x < 0.5, 0, 1)
 
   def generate(self):
-    songs = self.generator.predict(noise(batch_size))
+    songs = self.G.predict(noise(batch_size))
     return songs
-
-
-  def discriminate(self):
-    self.generator.predict()
-
 
   def noise(self, n, shape):
     return np.random.random((n,) + shape[1:])
