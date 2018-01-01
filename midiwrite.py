e2015d2a2409cdf357c5a9a3ca125754da75560c
d <dennis.magnusson98@gmail.com>
2018-01-01 17:57:22 +0100

    Fix midiwrite

--- midiwrite.py
+++ midiwrite.py
@@ -8,7 +8,6 @@ def write_midi(pattern):
   time = 0
   m.append(midi.KeySignatureEvent(tick=0, data=[0, 0]))
   m.append(midi.SetTempoEvent(tick=0, data=[13, 59, 231]))
-  m.append(midi.ControlChangeEvent(A
 
   m.append(midi.ControlChangeEvent(tick=0, channel=1, data=[91, 127]))
   m.append(midi.ControlChangeEvent(tick=0, channel=1, data=[10, 64]))
@@ -19,11 +18,10 @@ def write_midi(pattern):
   for frame in pattern:
     for i in range(len(frame)):
       if frame[i] == 1:
-        m.append([NoteOnEvent(tick=time, channel=1, data=[i+20, 100])])
+        m.append(midi.NoteOnEvent(tick=time, channel=1, data=[i+20, 100]))
 
 
     time += delta
 
-  m.append(midi.EndTrackEvent(tick=0, data=[]))
-  m.make_ticks_rel()
-  midi.write_midifile(midi.Pattern(foramt=0, resolution=480, tracks=[midi.Trakc[m]]))
+  m.append(midi.EndOfTrackEvent(tick=time, data=[]))
+  midi.write_midifile(midifile="test.mid", pattern=midi.Pattern(format=0, tick_relative=False, resolution=480, tracks=[midi.Track(events=m)]))
