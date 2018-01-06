3d77d0f59d435f7ad10bc1607d17c6b22be758aa
denmag <dennis.magnusson98@gmail.com>
2018-01-06 12:28:53 -0800

    Cleanup

--- preprocessor.py
+++ preprocessor.py
@@ -11,35 +11,21 @@ def read_dataset(directory, length=-1):
   songs = [midiparse.readfile_midi("data/"+f) for f in files]
   dataset = []
   for song in songs:
+    if length != -1 and len(dataset) > length:
+      break
+
     for div in divide_song(song, 64):
       dataset.append(div)
   
-  if length != -1:
-    dataset = dataset[:length]
 
   dataset = np.array(dataset)
   l = dataset.shape[0]
   dataset = dataset.reshape(l, 64, 88, 1).swapaxes(1,2)
   return dataset
   
-#TODO Test
-#TODO What to do with the last part?
 def divide_song(song, sequence_len):
   s = []
   for i in range(0, len(song)-sequence_len, sequence_len):
     s.append(song[i:i+sequence_len])
 
   return s
-
-def next_batch(dataset, index, batch_size):
-  for song in dataset:
-    if index < len(song):
-      break
-    else:
-      index -= len(song)
-    index -= 1
-  return False
-  #TODO How should I do this?
-  #One bar at a time?
-  #One sequence_length sequence at a time?
-  #One note at a time?
