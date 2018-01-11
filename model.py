e9fa9a6623d4225801827ef4c578c18d07ce9292
denmag <dennis.magnusson98@gmail.com>
2018-01-11 12:12:31 -0800

    Works decently on MNIST now

--- model.py
+++ model.py
@@ -9,6 +9,9 @@ from keras.models import Sequential, Model
 from keras.layers import Conv2D, Conv2DTranspose, Activation, Dense, Flatten, Dropout, Input, BatchNormalization, UpSampling2D, Reshape
 from keras.optimizers import Adam, RMSprop
 
+#TODO DELETEME
+from keras.datasets import mnist
+
 import preprocessor
 
 datasetino = []
@@ -16,25 +19,29 @@ datasetino = []
 class GAN:
   def __init__(self):
     global datasetino
-    self.D = self.create_discriminator([(12, 12), (6, 6), (4, 4)], [1, 5, 10], dropout=0.6)
-    self.D.compile(loss='binary_crossentropy', optimizer=RMSprop(0.001))
+    self.D = self.create_discriminator([(2, 2), (2, 2), (2, 2)], [1, 5, 10], dropout=0.7)
+    self.D.compile(loss='binary_crossentropy', optimizer=Adam(0.0001))
     self.D.summary()
-    self.G = self.create_generator([(12, 12), (12, 12), (12, 12)], [50, 25, 1])
-    self.G.compile(loss='binary_crossentropy', optimizer=RMSprop(0.0005))
+    self.G = self.create_generator([(5, 5), (5, 5), (5, 5)], [200, 100, 50, 1])
+    self.G.compile(loss='binary_crossentropy', optimizer=Adam(0.001))
     self.G.summary()
     inp = Input(shape=self.input_shape[1:])
 
-    self.D.trainable = False
+    #self.D.trainable = False
+    self.set_d_trainable(False)
     self.GAN = Model(inp, self.D(self.G(inp)))
-    self.GAN.compile(loss="binary_crossentropy", optimizer=RMSprop(0.1))
+    self.GAN.compile(loss="binary_crossentropy", optimizer=Adam(0.001))
 
   def run(self):
-    data = preprocessor.read_dataset("./data", length=-1)
-    datasetino = data
-    batch_size = 10
+    #data = preprocessor.read_dataset("./data", length=-1)
+    #data = preprocessor.read_dataset("./data", length=1000)
+    #datasetino = data
+    data = mnist.load_data()[0][0].reshape(60000, 28, 28, 1)
+    batch_size = 50
     print("pre-training")
-    self.pre_train(data[:1000], batch_size, epochs=2)
-    self.train(data[1000:], batch_size, epochs=20)
+    #self.pre_train(data[:1000], batch_size, epochs=5)#Overfitting this bad boy
+    self.pre_train(data[:1000], batch_size, epochs=5)#Overfitting this bad boy
+    self.train(data[100:], batch_size, epochs=20)
 
   def train(self, dataset, batch_size, epochs=1):
     print(dataset.shape)
@@ -52,20 +59,22 @@ class GAN:
       for i in range(0, dataset_length-batch_size, batch_size):
         noise = self.noise(batch_size, self.input_shape)
         gen_songs = self.G.predict(noise)#Generate songs
-        gen_songs = self.round(gen_songs)
+        #gen_songs = self.round(gen_songs) #TODO Uncomment?
         predictions = self.D.predict(gen_songs)
         other_predictions = self.D.predict(dataset[i:i+batch_size])
 
         G_loss = 1-self.D.train_on_batch(gen_songs, np.zeros((batch_size, 1)))
         D_loss = self.D.train_on_batch(dataset[i:i+batch_size], np.ones((batch_size, 1)))
 
-        GAN_loss = self.GAN.train_on_batch(noise, predictions)
+        #GAN_loss = self.GAN.train_on_batch(noise, predictions)
+        GAN_loss = self.GAN.train_on_batch(noise, np.ones((batch_size, 1)))
 
         total_G_loss += G_loss
         total_D_loss += D_loss
         total_GAN_loss += GAN_loss
 
         if i % 1500 == 0:
+          print()
           self.print_raw_song(gen_songs[0])
 
         print("{}/{} G_loss={}, D_loss={}, GAN_loss={}".format(i, dataset_length, G_loss, D_loss, GAN_loss), end='\r')
@@ -87,20 +96,46 @@ class GAN:
 
   #Give the generator a head start.
   def pre_train(self, dataset, batch_size, epochs=1):
+    #self.D.trainable = True
+    self.set_d_trainable(True)
     dataset_length = dataset.shape[0]
 
     for e in range(epochs):
       loss = 0
+      sum_loss = 0
       prev = 0
       for i in range(0, dataset_length-batch_size, batch_size):
         prev = loss
-        loss = self.G.train_on_batch(self.noise(batch_size, self.input_shape), dataset[i:i+batch_size])
-        print(loss, i, "/", dataset_length, "delta:", loss-prev, end="\r")
-    
-      print("Last loss= ", loss)
+        noise = self.noise(batch_size, self.input_shape)
+        #loss = self.G.train_on_batch(noise, dataset[i:i+batch_size])
+        gen_songs = self.G.predict(noise)
+        sum_loss += loss
+
+        x = np.concatenate((dataset[i:i+batch_size], gen_songs))
+        y = np.concatenate((np.ones((batch_size, 1)),  np.zeros((batch_size, 1))))
+        self.D.fit(x, y, shuffle=True, verbose=0)
+        d_loss = 0
+        #TODO This is not training
+        #TODO Probably just trips to one value. Shuffle?
+        #d_loss = 0.5*self.D.train_on_batch(dataset[i:i+batch_size], np.ones((batch_size, 1)))
+        #d_loss+= 0.5*self.D.train_on_batch(gen_songs, np.zeros((batch_size, 1)))
+
+        print(loss, i, "/", dataset_length, "d_loss:", d_loss, end="\r")
+
+      sum_loss /= (dataset_length-batch_size)/batch_size
+      if sum_loss < 0.02:
+        break
+      print("Avg loss= ", sum_loss)
     song = self.G.predict(self.noise(1, self.input_shape))
     self.print_raw_song(song[0])
+    #self.D.trainable = False
+    self.set_d_trainable(False)
 
+  def set_d_trainable(self, t):
+    self.D.trainable = t
+    for layer in self.D.layers:
+      layer.trainable = t
+  
   #XXX this prints sideways.
   def print_raw_song(self, song):
     for part in song:
@@ -115,10 +150,11 @@ class GAN:
 
   def create_discriminator(self, filter_sizes, n_filters, dropout=0.6):
     model = Sequential()
-    model.add(Conv2D(n_filters[1], filter_sizes[1], input_shape=(88, 64, 1)))
+    #model.add(Conv2D(n_filters[1], filter_sizes[1], input_shape=(88, 64, 1)))
+    model.add(Conv2D(n_filters[1], filter_sizes[1], input_shape=(28, 28, 1), padding='same'))
 
     for i in range(1, len(filter_sizes)):
-      model.add(Conv2D(n_filters[i], filter_sizes[i]))
+      model.add(Conv2D(n_filters[i], filter_sizes[i], padding='same'))
       model.add(Dropout(dropout))
       model.add(Activation('relu'))
 
@@ -129,18 +165,23 @@ class GAN:
 
     return model
     
+  #TODO You can't even overfit this piece of shit.
   def create_generator(self, filter_sizes, n_filters):
     model = Sequential()
-    self.input_shape=(1, 100)
-    model.add(Dense(22*16*100, input_shape=self.input_shape[1:]))
-    model.add(Reshape((22, 16, 100)))
+    self.input_shape=(1, n_filters[0])
+    #model.add(Dense(22*16*100, input_shape=self.input_shape[1:]))#TODO Uncomment
+    model.add(Dense(7*7*n_filters[0], input_shape=self.input_shape[1:]))#TODO Remove
+    model.add(BatchNormalization())
+    #model.add(Reshape((22, 16, 100)))#TODO Uncomment
+    model.add(Reshape((7, 7, n_filters[0])))#TODO Remove
     #model.add(UpSampling2D((2, 2), input_shape=self.input_shape[1:]))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(50, (12, 12), padding='same'))
-    model.add(Activation('relu'))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(25, (4, 4), padding='same'))
-    model.add(Activation('relu'))
+    model.add(UpSampling2D((4, 4)))
+    model.add(Conv2D(n_filters[1], (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('sigmoid'))
+    model.add(Conv2D(n_filters[2], (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('sigmoid'))
     model.add(Conv2D(1, (1, 1), padding='same'))
     model.add(Activation('sigmoid'))
 
