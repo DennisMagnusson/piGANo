526d676d167dd24c6acef9a013265de2f1dea26f
denmag <dennis.magnusson98@gmail.com>
2018-01-19 12:56:02 -0800

    Small cleanup of midiparse

--- midiparse.py
+++ midiparse.py
@@ -1,9 +1,16 @@
 from __future__ import print_function
 import midi
 
+import numpy as np
+
 #Does not use music21. Also way faster
 def readfile_midi(filename):
-  pattern = midi.read_midifile(filename)
+  try:
+    pattern = midi.read_midifile(filename)
+  except Warning:
+    print("Issue with", filename)
+    return []
+
   pattern.make_ticks_abs()
   pattern.sort()
   pattern = [list(filter(lambda x: type(x) == midi.NoteOnEvent, p)) for p in pattern]
@@ -22,16 +29,18 @@ def readfile_midi(filename):
     for event in pattern[q]:
       if event.tick > time:
         for i in range(time, event.tick, time_delta):#Fix time
-          #time += (event.tick - time) / time_delta
           time += time_delta
-          song.append([0]*88)
+          song.append(np.zeros(88, dtype='uint8'))
 
       if len(event.data) < 2:
         print(event)
         continue
         
       if event.data[1] != 0:
-        song.append([0]*88)
+        song.append(np.zeros(88, dtype='uint8'))
+        if event.data[0] < 21 or event.data[0] >= 88+21:
+          print(filename, "has note #", event.data[0]-21)
+          return []
         song[len(song)-1][event.data[0]-21] = 1
   
   trim(song)
@@ -39,13 +48,13 @@ def readfile_midi(filename):
 
 def trim(song):
   for i in song:
-    if i == [0]*88:
+    if np.all(i==0):
       song.pop(0)
     else:
       break
 
   for i in reversed(song):
-    if i == [0]*88:
+    if np.all(i==0):
       song.pop()
     else:
       break
