fde3bd686ade5e588e3957896c748a31f48efb90
denmag <dennis.magnusson98@gmail.com>
2018-05-07 11:05:03 -0700

    Haxx

--- model.py
+++ model.py
@@ -21,12 +21,12 @@ datasetino = []
 class GAN:
   def __init__(self):
     global datasetino
-    self.D = self.create_discriminator([(12, 16), (7, 1)], [64, 128], dropout=0.3)
+    self.D = self.create_discriminator([(12, 8), (7, 1)], [32, 64], dropout=0.3)
     self.D.compile(loss='binary_crossentropy', optimizer=Adam(0.0001))
     self.D.summary()
 
-    self.G = self.create_generator([(12, 12), (7, 7)], [10, 64])
-    self.G.compile(loss='binary_crossentropy', optimizer=Adam(0.01))
+    self.G = self.create_generator([(12, 12), (7, 7)], [64, 100])
+    self.G.compile(loss='binary_crossentropy', optimizer=Adam(0.001))
     self.G.summary()
     inp = Input(shape=self.input_shape[1:])
 
@@ -156,16 +156,17 @@ class GAN:
   def create_discriminator(self, filter_sizes, n_filters, dropout=0.6):
     model = Sequential()
     model.add(Conv2D(n_filters[0], filter_sizes[0], input_shape=(88, 64, 1), padding='same'))
+    model.add(AveragePooling2D(pool_size=(2, 2)))#TODO Remove?
     model.add(Dropout(dropout))
-    #model.add(LeakyReLU(0.2))
-    model.add(Activation('relu'))
+    model.add(LeakyReLU(0.2))
     #model.add(Conv2D(n_filters[1], filter_sizes[1], input_shape=(28, 28, 1), padding='same'))
 
     for i in range(1, len(filter_sizes)):
       model.add(Conv2D(n_filters[i], filter_sizes[i], padding='same'))
+      #model.add(AveragePooling2D(pool_size=(2, 2)))
       model.add(Dropout(dropout))
-      #model.add(LeakyReLU(0.2))
-      model.add(Activation('relu'))
+      model.add(LeakyReLU(0.2))
+      #model.add(Activation('leakyrelu'))
 
     #model.add(AveragePooling2D(1, 2))#TODO Doesn't work with this
     model.add(Flatten())
@@ -182,16 +183,16 @@ class GAN:
     model.add(Dropout(0.5, input_shape=self.input_shape[1:]))#TODO Is this good?
     model.add(Dense(22*16*10))
     #model.add(Dense(22*16*10, input_shape=self.input_shape[1:]))
-    model.add(Activation('relu'))
-    #model.add(LeakyReLU(0.2))
+    #model.add(Activation('leakyrelu'))
+    model.add(LeakyReLU(0.2))
     model.add(BatchNormalization())
     model.add(Reshape((22, 16, 10)))
     model.add(UpSampling2D((4, 4)))
     for i in range(1, len(filter_sizes)):
       model.add(Conv2D(n_filters[i], filter_sizes[i], padding='same'))
       model.add(BatchNormalization())
-      #model.add(LeakyReLU(0.2))
-      model.add(Activation('relu'))
+      model.add(LeakyReLU(0.2))
+      #model.add(Activation('leakyrelu'))
 
     model.add(Conv2D(1, (1, 1), padding='same'))
     model.add(Activation('sigmoid'))
@@ -207,5 +208,5 @@ class GAN:
     return songs
 
   def noise(self, n, shape):
-    return np.random.random((n,) + shape[1:])
+    return np.random.normal((n,) + shape[1:])
 
